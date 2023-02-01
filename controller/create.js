eb921d6d6060588fa9cfc085ef669a08da0eab91
Heni <henishreesoftic.com>
2023-02-01 15:50:37 +0530

    second create

--- controller/create.js
+++ controller/create.js
@@ -5,10 +5,6 @@ exports.create=async(req,res)=>{
     const user= new User(req.body)
     console.log("userrrsrsesr..",user)
     
-    if (!(title && date && authername && imgsrc)) {
-        throw new Error("All input is required");
- 
-     }
     try {
         await user.save()
         res.status(200).send(user)
