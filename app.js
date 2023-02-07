021a83daf9ef793a9d753bb072ba2a980aa56733
prince <princeshreesoftic.com>
2023-02-07 09:49:59 +0530

    upload img finalcord

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
-const express = require('express')
-const cors=require('cors')
-const path = require('path')
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
 require("./database").connect();
-const userRouter = require('./router/user')
+const userRouter = require("./router/user");
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
+app.use(userRouter);
 
-var multer = require('multer');
-var fs = require('fs');
-
-//. . . 
-const port=3001
-app.listen(port, ()=>{
-    console.log('We are live on' + port);
-    });
-
-var upload = multer({dest:'public\images'});
-
- app.post('/post', upload.single('file'), function(req, res) {
-  console.log(req.file);
- res.send("file saved on server");
- });
-
-app.use("/static",express.static(path.join(__dirname, 'public')))
-app.use(userRouter)
-
-const path = require("path");
-app.use(express.static(path.join(__dirname, '/public/images')));
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
--- router/user.js
+++ router/user.js
@@ -3,7 +3,7 @@ const { route } = require("../app");
 const { create } = require("../controller/create");
 const { deleteuser } = require("../controller/delete");
 const { getuserAll } = require("../controller/getall");
-const { upload } = require("../static/public/imgupload");
+const { uploads, multerUpload } = require("../controller/imgupload");
 const { loginform } = require("../controller/login");
 const { registrationform } = require("../controller/signup");
 const { updateuser } = require("../controller/update");
@@ -11,13 +11,15 @@ const { updateuser } = require("../controller/update");
 
 
 const router=new express.Router()
-
 router.post('/create',create)
 router.delete('/delete/:id',deleteuser)
 router.get("/getall",getuserAll)
 router.patch("/update/:id",updateuser)
 router.post('/ragister',registrationform)
 router.post('/login',loginform)
-router.post('/upload',upload)
+router.post('/upload',multerUpload,uploads)
+
+  
+
 module.exports=router
 
