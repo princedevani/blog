c6c38fdc96f1e2ac65ff0027144c2197f668b7b1
Heni <henishreesoftic.com>
2023-02-07 09:57:42 +0530

    img uploadind code pull

--- static/public/imgupload.js
+++ static/public/imgupload.js
@@ -4,62 +4,28 @@ const User=require('../../model/blogschema')
  
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
-    `images/Ss1.jpg`
-    callback(null, '/public/images');
+    // `images/Ss1.jpg`
+    callback(null, './static/public/images');
 
   },
   filename: function (req, file, callback) {
     console.log(file)
-    callback(null, file.fieldname + '-' + '.jpg');
+    // callback(null, file.fieldname + '-' + '.jpg');
+    callback(null, file.originalname);
   }
 });
-var uploadmulter = multer({ storage : storage}).single('upload');
+var uploadmulter = multer({ storage : storage}).single('img');
  
 exports.upload=function(req,res){
-  uploadmulter(req,res,function(err) {
-  // try {
-  //       console.log("secc uploading file")
-  //       res.end({imgsrc:""});
-  //     } catch (error) {
-  //       console.log("error uploading file")
-  //       // return res.end("Error uploading file.",err);
-  //       return res.end("Error uploading file.");
-  //     }  
-      if(err) {
-          console.log("error uploading file")
-            // return res.end("Error uploading file.",err);
-            return res.end("Error uploading file.");
-        }
-        console.log("secc uploading file")
-        res.end({imgsrc:""});
-        // res.status(200).seand("File is uploaded",uploadmulter)
-    });
-};
-
-
-//==============================================================================
-// exports.upload = async (req, res) => {
-//     try {
-//       const imgupload= await upload.any('upload');
-//       console.log(req.file);
-  
-//     //   if (req.file == undefined) {
-//     //     console.log("undisididi",req.file   );
-//     //     return res.send({
-//     //       message: "You must select a file.",
-//     //     });
-//     //   }
-  
-//       console.log("successfully uploaded",imgupload)
-//       return res.send({
-//         message: "File has been uploaded.",imgupload
-//       });
-//     } catch (error) {
-//       console.log(error);
-  
-//       return res.send({
-//         message: "Error when trying upload image: ${error}",
-//       });
-//     }
-//   };
-  
+  // uploadmulter(req,res,function(err) {
+    uploadmulter(req,res,function(err) {
+  try {
+    res.send({imgsrc:`/public/images`+ file.originalname});
+    console.log("secc uploading file")
+      } catch (error) {
+        console.log("error uploading file")
+        // return res.end("Error uploading file.",err);
+        return res.end("Error uploading file.");
+      }  
+})
+}
